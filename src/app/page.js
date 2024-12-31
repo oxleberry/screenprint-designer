14dd51d2cf7df2c94784082f5ace311e0f67c614
Sharon <sharon.paz.00@gmail.com>
2024-12-31 08:57:18 -0800

    feat(screenprint designer): add background color picker

--- src/app/page.js
+++ src/app/page.js
@@ -1,96 +1,48 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import { useEffect, useState, useRef } from 'react'
+
 import '../styles/globals.scss';
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+export default function ScreenprintDesigner() {
+	// States =================
+	const [backgroundColor, setBackgroundColor] = useState('#000000');
+
+	// Functions =================
+	function backgroundColorHandler(event) {
+		let value = event.target.value;
+		setBackgroundColor(value);
+	}
+
+
+	return (
+		<div className="full-backboard screenprint-designer-page">
+			<h1>Screenprint Designer</h1>
+			<main className="screenprint-designer-page">
+				<section className="share-content-section">
+					<div className="share-content-container" style={{background: `${backgroundColor}`}}>
+						<h2 className="hidden">Share Content</h2>
+					</div>
+				</section>
+
+				{/* Option Section */}
+				<section className="options-container">
+					<h2 className="hidden">Options</h2>
+					{/* Option Pick a Color */}
+					<div className="option option-color">
+						<label htmlFor="custom-color" className="option-label">Pick a background color:</label>
+						<input
+							id="custom-color"
+							className="custom-color"
+							name="custom-color"
+							type="color"
+							value={backgroundColor}
+							onChange={backgroundColorHandler}
+						/>
+					</div>
+				</section>
+
+			</main>
+		</div>
+	);
 }
